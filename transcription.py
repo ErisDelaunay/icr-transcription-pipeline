6f958059786d9c5aabeb8fe1f6d085674076d579
ErisDelaunay <ema.nieddu@gmail.com>
2019-07-01 16:06:56 +0200

    pathlib, deduplication

--- transcription.py
+++ transcription.py
@@ -1,6 +1,7 @@
 import itertools
 import cv2
-import os
+import pathlib
+# import os
 import shutil
 import json
 import kenlm
@@ -14,7 +15,6 @@ from math import inf
 from evaluation import evaluate_word_accuracy, mrr, correct_transcr_count
 from absl import app, flags
 
-from pprint import pprint
 
 """
 HELPER FUNCTIONS
@@ -99,7 +99,7 @@ def _compute_segments_and_centroids(word_img, bg_color=(255, 255, 255)):
         w = x2 - x1
         h = y2 - y1
 
-        if w * h > 9:
+        if w * h > 0:
             cx, cy = ctds[1]
             centroids.append(
                 (int(np.rint(cx)), int(np.rint(cy)))
@@ -337,30 +337,19 @@ APP MAIN
 import matplotlib.pyplot as plt
 
 def main(unused_argv):
-    # set destination folder names
-    # sfx = _dst_suffix()
-    tsc_dir, dag_dir, eval_fnm = 'tsc', 'dag', 'eval'
+    src_dir = pathlib.Path(flags.FLAGS.src_dir)
+    dst_dir = pathlib.Path(flags.FLAGS.dst_dir)
 
-    # make necessary directories
-    if os.path.isdir(tsc_dir):
-        shutil.rmtree(tsc_dir)
-    os.mkdir(tsc_dir)
-
-    if os.path.isdir(dag_dir):
-        shutil.rmtree(dag_dir)
-    os.mkdir(dag_dir)
-
-    all_classes = [ # '1_not_char', '0_con',
-        '0_b_stroke', '_curl', '0_d_stroke', '0_l_stroke',
-        '0_nt',       '0_per',  '0_pro', '0_qui',      '0_rum',     '0_semicolon',
-        'a', 'b', 'c', 'd', 'e',      'f',        'g', 'h', 'i', 'l', 'm', 'n',
-        'o', 'p', 'q', 'r', 's_alta', 's_ending', 't', 'u', 'x'
-    ]
-    clrs = cv2.imread('palette2.png')[0]
-
+    # all_classes = [ # '1_not_char', '0_con',
+    #     '0_b_stroke', '_curl', '0_d_stroke', '0_l_stroke',
+    #     '0_nt',       '0_per',  '0_pro', '0_qui',      '0_rum',     '0_semicolon',
+    #     'a', 'b', 'c', 'd', 'e',      'f',        'g', 'h', 'i', 'l', 'm', 'n',
+    #     'o', 'p', 'q', 'r', 's_alta', 's_ending', 't', 'u', 'x'
+    # ]
+    # clrs = cv2.imread('palette2.png')[0]
 
     icr_classifier = keras.models.load_model(
-        flags.FLAGS.ocr_dir,
+        flags.FLAGS.ocr,
         custom_objects={
             'custom_loss':custom_loss,
             'char_accuracy':char_accuracy,
@@ -370,166 +359,175 @@ def main(unused_argv):
 
     # load the Language Model
     global model_LM
-    if flags.FLAGS.n_gram == 0:
+    if flags.FLAGS.lm == 'noLM':
         model_LM = None
     else:
-        model_LM = kenlm.Model(
-            os.path.join(
-                flags.FLAGS.lm_dir,
-                'corpus_%dgram.arpa' % flags.FLAGS.n_gram
-            )
-        )
+        model_LM = kenlm.Model(flags.FLAGS.lm)
 
-    start_time = time()
-    words = os.listdir(flags.FLAGS.word_dir)
+    for page_path in src_dir.glob('*'):
+        start_time = time()
 
-    for word in words:
-        word_img = cv2.imread(os.path.join(flags.FLAGS.word_dir, word))
-        _, word_img_bin = cv2.threshold(cv2.cvtColor(word_img, cv2.COLOR_BGR2GRAY), 254, 255, cv2.THRESH_BINARY_INV)
-        _, _, stats, _ = cv2.connectedComponentsWithStats(word_img_bin)
-        x1, y1, x2, y2 = _compute_bbx(stats)
-        word_img_crop = word_img[y1:y2,x1:x2]
+        tsc_dir = dst_dir / page_path.stem
+        tsc_dir.mkdir(parents=True, exist_ok=True)
 
-        segments, centroids = _compute_segments_and_centroids(word_img_crop)
+        for word_path in page_path.glob(flags.FLAGS.img_dir+'/*'):
+            word_img = cv2.imread(str(word_path))
+            _, word_img_bin = cv2.threshold(cv2.cvtColor(word_img, cv2.COLOR_BGR2GRAY), 254, 255, cv2.THRESH_BINARY_INV)
+            _, _, stats, _ = cv2.connectedComponentsWithStats(word_img_bin)
+            x1, y1, x2, y2 = _compute_bbx(stats)
+            word_img_crop = word_img[y1:y2,x1:x2]
 
-        sorted_segments, sorted_centroids = zip(
-            *sorted(
-                zip(segments, centroids),
-                key=lambda x: (x[1][0], x[1][1])
-            )
-        )
+            segments, centroids = _compute_segments_and_centroids(word_img_crop)
 
-        # generate all possible segment combinations
-        segments_and_centroids_combinations = [
-            (sorted_segments[sorted_centroids.index((x1, y1)):sorted_centroids.index((x2, y2)) + 1],
-             sorted_centroids[sorted_centroids.index((x1, y1)):sorted_centroids.index((x2, y2)) + 1])
-            for (x1, y1), (x2, y2) in itertools.combinations_with_replacement(sorted_centroids, 2)
-            if (x2 - x1) < 25
-        ]
-
-        grouped_segments, centroid_ids = zip(*segments_and_centroids_combinations)
-
-        # create samples for prediction
-        X_test = np.array([_make_sample(s) for s in grouped_segments], dtype='float32') / 255
-
-        logit_preds = icr_classifier.predict(X_test)
-
-        char_preds = tf.nn.softmax(logit_preds[:, 1:], axis=1).numpy()
-        notchar_preds = tf.nn.sigmoid(logit_preds[:, :1]).numpy()
-
-        # filter segment combinations according to classification
-        filtered_combinations = []
-        for i, cc in enumerate(centroid_ids):
-            if notchar_preds[i] <= flags.FLAGS.notchar_thr:
-                s_mask = np.zeros(grouped_segments[i][0].shape, dtype='uint8')
-                for s in grouped_segments[i]:
-                    s_mask = cv2.bitwise_or(s_mask, s)
-                filtered_combinations.append((cc, char_preds[i], s_mask))
-
-        print(
-            '{}:\nKept: {} out of {} potential edges ({:.2f}%)'.format(
-                word,
-                len(filtered_combinations),
-                len(centroid_ids),
-                (len(filtered_combinations)/len(centroid_ids)) * 100
-            )
-        )
-
-        # creation of the word lattice: segment combinations represent
-        # edges. Nodes are segments consumed up to a certain point.
-        lattice = nx.DiGraph()
-
-        filtered_combinations = sorted(filtered_combinations, key=lambda x: x[0][0])
-        filtered_centroids =  sorted({c for ctds, _, _ in filtered_combinations for c in ctds})
-
-        nodes = [
-            set(filtered_centroids[:i])
-            for i in range(len(filtered_centroids) + 1)
-        ]
-        edges = [
-            set(ctds)
-            for ctds, _, _ in filtered_combinations
-        ]
-
-        for u, v in itertools.combinations(nodes, 2):
-            if v - u in edges:
-                _, preds, sgmt_img = filtered_combinations[edges.index(v-u)]
-                lattice.add_edge(
-                    tuple(sorted(u)),
-                    tuple(sorted(v)),
-                    preds=preds,
-                    image=sgmt_img
+            sorted_segments, sorted_centroids = zip(
+                *sorted(
+                    zip(segments, centroids),
+                    key=lambda x: (x[1][0], x[1][1])
                 )
+            )
 
-        print("nodes: {},\tedges: {}\n".format(len(lattice.nodes()), len(lattice.edges())))
+            # generate all possible segment combinations
+            segments_and_centroids_combinations = [
+                (sorted_segments[sorted_centroids.index((x1, y1)):sorted_centroids.index((x2, y2)) + 1],
+                 sorted_centroids[sorted_centroids.index((x1, y1)):sorted_centroids.index((x2, y2)) + 1])
+                for (x1, y1), (x2, y2) in itertools.combinations_with_replacement(sorted_centroids, 2)
+                if (x2 - x1) < 25
+            ]
 
-        # # save a .js file with the lattice structure
-        # dict_nodes = [
-        #     str([sorted_centroids.index(c) for c in node])
-        #     for node in lattice.nodes()
-        # ]
-        #
-        # lattice_dict = {
-        #     'nodes': sorted(dict_nodes, reverse=True),
-        #     'edges': []
-        # }
-        #
-        # graph_as_dict = nx.to_dict_of_dicts(lattice)
-        #
-        # for src_node in graph_as_dict:
-        #     for dst_node in graph_as_dict[src_node]:
-        #         labels = ''
-        #         for edge_ix in graph_as_dict[src_node][dst_node]:
-        #             labels += '%s: %.4f\n' % (graph_as_dict[src_node][dst_node][edge_ix]['transcription'],
-        #                                       graph_as_dict[src_node][dst_node][edge_ix]['weight'])
-        #         dict_src_node = str([sorted_centroids.index(c) for c in src_node])
-        #         dict_dst_node = str([sorted_centroids.index(c) for c in dst_node])
-        #         lattice_dict['edges'].append((dict_src_node, dict_dst_node, labels))
-        #
-        # with open(os.path.join(dag_dir, word.replace('/', '_').split('.')[0] + '.js'), 'w') as f:
-        #     f.write('var graph = ' + json.dumps(lattice_dict, indent=2))
+            grouped_segments, centroid_ids = zip(*segments_and_centroids_combinations)
 
-        # Transcription generation:
-        transcriptions = set()
+            # create samples for prediction
+            X_test = np.array([_make_sample(s) for s in grouped_segments], dtype='float32') / 255
 
-        if len(lattice.nodes()) > 0:
-            for path in paths_beam(
-                    lattice,
-                    source=sorted(lattice.nodes())[0],
-                    targets={sorted(lattice.nodes())[-1]},
-                    width=3,
-                    lm_th=flags.FLAGS.lm_thr):
-                transcript = ''
-                prob = 0.0
-                for _, char, score in path[1:]:
-                    transcript += char
-                    prob += score
-                transcript = transcript.replace('b;', 'bus').replace('q;', 'que').replace('qui;', 'que')
-                transcript = transcript.strip(';')
-                transcriptions.add((prob, transcript, str(path)))
+            logit_preds = icr_classifier.predict(X_test)
 
-        transcriptions = sorted(transcriptions, reverse=True)
+            char_preds = tf.nn.softmax(logit_preds[:, 1:], axis=1).numpy()
+            notchar_preds = tf.nn.sigmoid(logit_preds[:, :1]).numpy()
 
-        with open(os.path.join(tsc_dir, word.replace('/', '_').split('.')[0] + '.txt'), 'w') as f:
-            for t in transcriptions:
-                f.write(str(t) + '\n')
+            # filter segment combinations according to classification
+            filtered_combinations = []
+            for i, cc in enumerate(centroid_ids):
+                if notchar_preds[i] <= flags.FLAGS.notchar_thr:
+                    s_mask = np.zeros(grouped_segments[i][0].shape, dtype='uint8')
+                    for s in grouped_segments[i]:
+                        s_mask = cv2.bitwise_or(s_mask, s)
+                    filtered_combinations.append((cc, char_preds[i], s_mask))
 
-    end_time = time()
+            print(
+                '{}:\nKept: {} out of {} potential edges ({:.2f}%)'.format(
+                    str(word_path),
+                    len(filtered_combinations),
+                    len(centroid_ids),
+                    (len(filtered_combinations)/len(centroid_ids)) * 100
+                )
+            )
 
-    # evaluation
-    elapsed = end_time - start_time
-    print('time elapsed:', elapsed)
-    print(tsc_dir)
+            # creation of the word lattice: segment combinations represent
+            # edges. Nodes are segments consumed up to a certain point.
+            lattice = nx.DiGraph()
 
-    MRR = mrr(tsc_dir, flags.FLAGS.gt_dir)
-    correct_count = correct_transcr_count(tsc_dir, flags.FLAGS.gt_dir)
-    evaluation = evaluate_word_accuracy(tsc_dir, flags.FLAGS.gt_dir)
-    evaluation['mrr'] = MRR
-    evaluation['time'] = elapsed
-    evaluation['correct_overall'] = correct_count
+            filtered_combinations = sorted(filtered_combinations, key=lambda x: x[0][0])
+            filtered_centroids =  sorted({c for ctds, _, _ in filtered_combinations for c in ctds})
 
-    print(json.dumps(evaluation, indent=2))
-    json.dump(evaluation, open(eval_fnm+'.json','w'), indent=2)
+            nodes = [
+                set(filtered_centroids[:i])
+                for i in range(len(filtered_centroids) + 1)
+            ]
+            edges = [
+                set(ctds)
+                for ctds, _, _ in filtered_combinations
+            ]
+
+            for u, v in itertools.combinations(nodes, 2):
+                if v - u in edges:
+                    _, preds, sgmt_img = filtered_combinations[edges.index(v-u)]
+                    lattice.add_edge(
+                        tuple(sorted(u)),
+                        tuple(sorted(v)),
+                        preds=preds,
+                        image=sgmt_img
+                    )
+
+            print("nodes: {},\tedges: {}\n".format(len(lattice.nodes()), len(lattice.edges())))
+
+            # # save a .js file with the lattice structure
+            # dict_nodes = [
+            #     str([sorted_centroids.index(c) for c in node])
+            #     for node in lattice.nodes()
+            # ]
+            #
+            # lattice_dict = {
+            #     'nodes': sorted(dict_nodes, reverse=True),
+            #     'edges': []
+            # }
+            #
+            # graph_as_dict = nx.to_dict_of_dicts(lattice)
+            #
+            # for src_node in graph_as_dict:
+            #     for dst_node in graph_as_dict[src_node]:
+            #         labels = ''
+            #         for edge_ix in graph_as_dict[src_node][dst_node]:
+            #             labels += '%s: %.4f\n' % (graph_as_dict[src_node][dst_node][edge_ix]['transcription'],
+            #                                       graph_as_dict[src_node][dst_node][edge_ix]['weight'])
+            #         dict_src_node = str([sorted_centroids.index(c) for c in src_node])
+            #         dict_dst_node = str([sorted_centroids.index(c) for c in dst_node])
+            #         lattice_dict['edges'].append((dict_src_node, dict_dst_node, labels))
+            #
+            # with open(os.path.join(dag_dir, word.replace('/', '_').split('.')[0] + '.js'), 'w') as f:
+            #     f.write('var graph = ' + json.dumps(lattice_dict, indent=2))
+
+            # Transcription generation:
+
+            transcriptions = set()
+
+            if len(lattice.nodes()) > 0:
+                for path in paths_beam(
+                        lattice,
+                        source=sorted(lattice.nodes())[0],
+                        targets={sorted(lattice.nodes())[-1]},
+                        width=3,
+                        lm_th=flags.FLAGS.lm_thr):
+                    transcript = ''
+                    prob = 0.0
+                    for _, char, score in path[1:]:
+                        transcript += char
+                        prob += score
+                    transcript = transcript.replace('b;', 'bus').replace('q;', 'que').replace('qui;', 'que')
+                    transcript = transcript.strip(';')
+                    transcriptions.add((prob, transcript, str(path)))
+
+            transcriptions = sorted(transcriptions, reverse=True)
+
+            transcriptions_filtered = []
+
+            for line in transcriptions:
+                if len(transcriptions_filtered) > 0:
+                    _, tfil, _ = zip(*transcriptions_filtered)
+                else:
+                    tfil = []
+                if line[1] not in tfil:
+                    transcriptions_filtered.append(line)
+
+            dst_tsc = tsc_dir / (word_path.stem  + '.txt')
+            with open(str(dst_tsc), 'w') as f:
+                for t in transcriptions_filtered:
+                    f.write(str(t) + '\n')
+
+        end_time = time()
+
+        # evaluation
+        elapsed = end_time - start_time
+        print('time elapsed:', elapsed)
+
+        MRR = mrr(str(tsc_dir), str(page_path / flags.FLAGS.tsc_dir))
+        correct_count = correct_transcr_count(str(tsc_dir), str(page_path / flags.FLAGS.tsc_dir))
+        evaluation = evaluate_word_accuracy(str(tsc_dir), str(page_path / flags.FLAGS.tsc_dir))
+        evaluation['mrr'] = MRR
+        evaluation['time'] = elapsed
+        evaluation['correct_overall'] = correct_count
+
+        print(json.dumps(evaluation, indent=2))
+        json.dump(evaluation, open(str(tsc_dir)+'_eval.json','w'), indent=2)
 
 # con moltiplicazione:
 # 050v 0.34 mrr: 0.17
@@ -555,15 +553,17 @@ def main(unused_argv):
 
 
 if __name__ == '__main__':
-    flags.DEFINE_integer('n_gram', 6, 'Language Model order')
+    # flags.DEFINE_integer('n_gram', 6, 'Language Model order')
+    # flags.DEFINE_float('char_thr', 0.1, 'character probability pruning threshold')
+    # flags.DEFINE_float('pdist_thr', 0.8, 'probability distribution pruning threshold')
+    # flags.DEFINE_integer('alt_top_n', 0, 'top n transcriptions to submit to alternative generation')
+    flags.DEFINE_string('src_dir', 'word_imgs/all_pages_clean', 'source folder')
+    flags.DEFINE_string('dst_dir', 'tsc_gen_clean_ncthr=0.5', 'destination folder')
+    flags.DEFINE_string('img_dir', 'images', 'word image folder name')
+    flags.DEFINE_string('tsc_dir', 'transcriptions', 'word transcription folder name')
+    flags.DEFINE_string('lm', 'lm_model/corpus_6gram.arpa', 'Language model file')
+    flags.DEFINE_string('ocr', 'ocr_model/new_multiout.hdf5', 'character classifier file')
+    flags.DEFINE_float('notchar_thr', 0.5, 'not character probability pruning threshold')
     flags.DEFINE_float('lm_thr', -16.0, 'LM probability pruning threshold')
-    flags.DEFINE_float('char_thr', 0.1, 'character probability pruning threshold')
-    flags.DEFINE_float('notchar_thr', 0.7, 'not character probability pruning threshold')
-    flags.DEFINE_float('pdist_thr', 0.8, 'probability distribution pruning threshold')
-    flags.DEFINE_integer('alt_top_n', 0, 'top n transcriptions to submit to alternative generation')
-    flags.DEFINE_string('lm_dir', 'lm_model', 'Language model folder')
-    flags.DEFINE_string('ocr_dir', 'ocr_model/new_multiout.hdf5', 'character classifier model folder')
-    flags.DEFINE_string('word_dir', 'word_imgs/042r_044v/044v_587_236_1355_1808/images', 'word image source folder')
-    flags.DEFINE_string('gt_dir', 'word_imgs/042r_044v/044v_587_236_1355_1808/transcriptions', 'ground truth source folder')
 
     app.run(main)
